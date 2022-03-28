6e0a365e186c147dc572e5822bb79e06137ab51a
Fazle Rabbi Faiyaz <63752799+fazlerabbi125@users.noreply.github.com>
2022-03-28 08:47:45 +0600

    Update About.vue

--- Vue projects/courier-service-app/src/views/About.vue
+++ Vue projects/courier-service-app/src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-  <h2  style="text-align:center;" >This is a To-Do App created using Vue JS</h2>
+  <div>
+  <h2  class="title" >This is a Courier Service App created using Vue JS</h2>
   </div>
 </template>
