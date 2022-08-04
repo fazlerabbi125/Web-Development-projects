1c1c2bfa18d6424f9c885f11d505741c6ab55c92
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2022-08-05 00:32:45 +0600

    React with TypeScript

--- React projects/employee-management/src/views/Home.tsx
+++ React projects/employee-management/src/views/Home.tsx
@@ -6,7 +6,6 @@ import EmployeeList from '../components/EmployeeList/EmployeeList';
 function Home() {
     const [query, setQuery] = React.useState<string>('');
     const { data:employeeList, error, isLoading }=useAxios('')
-    console.log(employeeList);
     return (
     <Layout>
         {isLoading && <h2 style={{color:"white",textAlign:"center" }}>Loading <i className="fa fa-spinner fa-spin"></i></h2>}
