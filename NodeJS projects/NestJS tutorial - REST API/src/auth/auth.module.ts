933562639e7f5e703d6154bfe69802885c3d9999
Fazle Rabbi Faiyaz <63752799+fazlerabbi125@users.noreply.github.com>
2022-08-14 16:42:39 +0600

    Update auth.module.ts

--- NodeJS projects/NestJS tutorial - REST API/src/auth/auth.module.ts
+++ NodeJS projects/NestJS tutorial - REST API/src/auth/auth.module.ts
@@ -3,12 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schemas/user.schema'; 
+import { UserSchema } from 'src/schemas/user.schema'; 
 
 @Module({
   imports: [
     JwtModule.register({}),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([{ name: "User", schema: UserSchema }])
   ],
   controllers: [AuthController],
   providers: [AuthService,],
